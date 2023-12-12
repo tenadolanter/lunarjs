fb532a6febfe30d5bacf8bfb1cfa9fc2640980fb
tenado <690784374@qq.com>
2023-12-12 14:41:21 +0800

    fix: 修复转换bug

--- example/index.js
+++ example/index.js
@@ -1,4 +1,5 @@
-import { solar2lunar } from "../src/index.js";
+import { solar2lunar, lunar2solar } from "../src/index.js";
 
-const data = solar2lunar();
-console.log(data);
+const data = solar2lunar(2023, 12, 12);
+const data1 = lunar2solar(2023, 11, 1);
+console.log(data, data1);
